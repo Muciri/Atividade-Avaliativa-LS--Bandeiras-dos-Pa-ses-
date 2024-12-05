260c512f697a9e363f84f92970907e903f5f6c6f
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2024-12-05 19:22:26 -0300

    correções

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function bandeira_card(flag){
     let card = 
     `
     <div class="flag col-2 my-2 text-center">
-        <img src="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${flag.id}.svg" alt="${flag.name}">
+        <img src="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${flag.id}.svg" alt="${flag.name}" style="height: 6REM;width: 6rem;">
         <p>${flag.name}</p>
     </div>
     `;
